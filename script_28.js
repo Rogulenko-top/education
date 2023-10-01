daa60ab745e8cfad8734b86dc19853c451a7aa65
Anton <rogulenkoaa@gmail.com>
2023-10-01 20:29:12 +0500

    lesson how use methode

---
+++ script_28.js
@@ -0,0 +1,25 @@
+"use strict"
+
+const str = "test";
+const arr = [1 , 2, 4]
+
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+
+let fruit = "Same fruit";
+
+console.log(fruit.indexOf("fruit"));
+
+const logg = "Hello world";
+
+console.log(logg.slice(6, 11));
+
+console.log(logg.substring(7, 11));
+
+
+const num = 12.2;
+console.log(Math.round(num));
+
+const tst = '12.8px';
+console.log(parseInt(tst));
+console.log(parseFloat(tst));
