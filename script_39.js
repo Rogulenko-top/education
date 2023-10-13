690df4f0a9bd86189f63436d50597a08d417a54f
Anton <rogulenkoaa@gmail.com>
2023-10-13 21:48:53 +0500

    lesson: data type conversion

---
+++ script_39.js
@@ -0,0 +1,48 @@
+'use strict'
+
+//To String 
+
+//1)
+console.log(typeof(String(null)));
+console.log(typeof(String(4)));
+
+//2)
+console.log(typeof(5 + '')); //конкатинация
+
+const num = 5;
+
+console.log('https://vk.com/catalog/' + num);
+
+const fontSize = 26 + 'px';
+
+//To Number
+//1)
+console.log(typeof(Number('4')));
+//2)
+console.log(typeof(+'5'));
+//3)
+console.log(typeof(parseInt('15px', 10)));
+
+let answer = +prompt('Hello', '');
+
+//To Boolean
+
+//0, '', null, undefined, NaN; - все это все false
+//1)
+let switcher = null;
+
+if (switcher) {
+	console.log('Working...');
+}
+
+switcher = 1;
+
+if (switcher) {
+	console.log('Working...');
+}
+
+//2)
+console.log(typeof(Boolean('4')));
+
+//3)
+console.log(typeof(!!'44444'));
